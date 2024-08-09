08d6441e1691a2b4478f1cabfa30af49bddc74ea
chenjl-ops <chenjl8891@126.com>
2024-08-09 16:27:59 +0700

    add send phone sms

--- nexmo/nexmo.go
+++ nexmo/nexmo.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"fmt"
 	"github.com/chenjl-ops/go-lib/requests"
+	"slices"
+	"strconv"
 )
 
 func New(apiKey string, apiSecret string) *Nexmo {
@@ -17,7 +19,7 @@ func New(apiKey string, apiSecret string) *Nexmo {
 	}
 }
 
-func (s *SmsSuccessResponse) validate() error {
+func (s *SmsMessageSuccessResponse) validate() error {
 	if s.MessageUUID == "" {
 		return errors.New("message uuid is empty")
 	}
@@ -44,7 +46,8 @@ func (n *Nexmo) GetAuthHeaders() map[string]string {
 	return headers
 }
 
-func (n *Nexmo) SendSms(from string, to string, message string) (map[string]interface{}, error) {
+// SendSmsMessage 向某个渠道发送sms
+func (n *Nexmo) SendSmsMessage(from string, to string, message string) (map[string]interface{}, error) {
 	url := "https://api.nexmo.com/v1/messages"
 
 	requestData := map[string]string{
@@ -55,8 +58,8 @@ func (n *Nexmo) SendSms(from string, to string, message string) (map[string]inte
 		"to":           to,
 	}
 
-	var successData SmsSuccessResponse
-	var failureData SmsFailureResponse
+	var successData SmsMessageSuccessResponse
+	var failureData SmsMessageFailureResponse
 	err := requests.RequestWithError(url, "POST", n.GetAuthHeaders(), requestData, &successData, &failureData)
 	if err != nil {
 		fmt.Println(err)
@@ -72,6 +75,47 @@ func (n *Nexmo) SendSms(from string, to string, message string) (map[string]inte
 	} else {
 		data["success"] = true
 		data["data"] = successData
+	}
+	return data, nil
+}
+
+// SendSMS 手机短信
+func (n *Nexmo) SendSMS(from string, to string, message string, messageType string) (map[string]interface{}, error) {
+	MessageTypes := []string{"json", "xml"}
+	if slices.Contains(MessageTypes, messageType) {
+		return nil, errors.New("message type is invalid: " + messageType)
+	}
+
+	url := fmt.Sprintf("https://api.nexmo.com/v1/%s", messageType)
+	requestData := map[string]string{
+		"from":       from,
+		"to":         to,
+		"text":       message,
+		"type":       "unicode",
+		"api_key":    n.ApiKey,
+		"api_secret": n.ApiSecret,
+	}
+
+	var smsData SmsBaseResponse
+	err := requests.Request(url, "POST", n.GetAuthHeaders(), requestData, &smsData)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	var data map[string]interface{}
+	number, err := strconv.Atoi(smsData.MessageCount)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	} else {
+		if (number >= 1) && (smsData.Messages[0].Status == "0") {
+			data["success"] = true
+		} else {
+			data["success"] = false
+			data["error"] = smsData.Messages[0].ErrorText
+		}
+		data["data"] = smsData
 	}
 	return data, nil
 }
--- nexmo/struct.go
+++ nexmo/struct.go
@@ -6,13 +6,30 @@ type Nexmo struct {
 	AuthCode  string `json:"auth_code"`
 }
 
-type SmsSuccessResponse struct {
+type SmsMessageSuccessResponse struct {
 	MessageUUID string `json:"message_uuid"`
 }
 
-type SmsFailureResponse struct {
+type SmsMessageFailureResponse struct {
 	Type     string `json:"type"`
 	Title    string `json:"title"`
 	Detail   string `json:"detail"`
 	Instance string `json:"instance"`
+}
+
+type SmsBaseData struct {
+	To               string `json:"to"`
+	MessageId        string `json:"message-id"`
+	Status           string `json:"status"`
+	RemainingBalance string `json:"remaining-balance"`
+	MessagePrice     string `json:"message-price"`
+	Network          string `json:"network"`
+	ClientRef        string `json:"client-ref"`
+	AccountRef       string `json:"account-ref"`
+	ErrorText        string `json:"error-text"`
+}
+
+type SmsBaseResponse struct {
+	MessageCount string        `json:"message-count"`
+	Messages     []SmsBaseData `json:"messages"`
 }
