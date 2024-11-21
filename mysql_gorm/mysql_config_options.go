f2a222a77c85bdfb55332f8358a8706703a5290c
陈继亮 <chenjl8891@126.com>
2024-11-21 12:18:46 +0800

    mysql db new add func collation

--- mysql_gorm/mysql_config_options.go
+++ mysql_gorm/mysql_config_options.go
@@ -2,22 +2,24 @@ package mysql_gorm
 
 // default mysql config
 const (
-	MYSQL_USERNAME = "root"
-	MYSQL_PASSWORD = "root"
-	MYSQL_HOST     = "localhost"
-	MYSQL_PORT     = 3306
-	MYSQL_DBNAME   = "test"
-	MYSQL_CHARSET  = "utf8mb4"
+	MYSQL_USERNAME  = "root"
+	MYSQL_PASSWORD  = "root"
+	MYSQL_HOST      = "localhost"
+	MYSQL_PORT      = 3306
+	MYSQL_DBNAME    = "test"
+	MYSQL_CHARSET   = "utf8mb4"
+	MYSQL_COLLATION = "utf8mb4_0900_ai_ci"
 )
 
 func NewDB(opts ...MysqlConfigOption) (*DB, error) {
 	result := &DB{
-		UserName: MYSQL_USERNAME,
-		Password: MYSQL_PASSWORD,
-		Host:     MYSQL_HOST,
-		Port:     MYSQL_PORT,
-		DBName:   MYSQL_DBNAME,
-		Charset:  MYSQL_CHARSET,
+		UserName:  MYSQL_USERNAME,
+		Password:  MYSQL_PASSWORD,
+		Host:      MYSQL_HOST,
+		Port:      MYSQL_PORT,
+		DBName:    MYSQL_DBNAME,
+		Charset:   MYSQL_CHARSET,
+		Collation: MYSQL_COLLATION,
 	}
 
 	//fmt.Println("init mysql config: ", result)
@@ -73,5 +75,12 @@ func WithHost(host string) MysqlConfigOption {
 func WithCharset(charset string) MysqlConfigOption {
 	return func(db *DB) {
 		db.Charset = charset
+	}
+}
+
+// WithCollation ...
+func WithCollation(collation string) MysqlConfigOption {
+	return func(db *DB) {
+		db.Collation = collation
 	}
 }
